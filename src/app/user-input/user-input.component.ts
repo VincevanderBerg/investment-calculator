1b0b5769fd41b949c2482f3f8e2600e21faa0f70
vincevanderberg <vincevanderberg@gmail.com>
2024-06-07 14:38:37 +0200

    feat: Enhance UserInputComponent form
    - Updated user-input.component.html to include step attributes for number inputs
      - Added step="0.01" for initial investment, annual investment, and expected return inputs
    - Added labels for better input field identification
    - Modified the duration label to specify years
    - Added an onSubmit() method in user-input.component.ts to handle form submission
      - Logs the entered values for initial investment, annual investment, expected return, and duration
    - Included a <div> in user-input.component.html to display entered values below the form

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 
 @Component({
@@ -11,5 +11,12 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  initial_investment = 0.0;
+  annual_investment = 0.0;
+  expected_return = 0.0;
+  duration = 0;
 
+  onSubmit() {
+    console.log('You entered values A: ' + this.initial_investment.toFixed(2) + ' and B: ' + this.annual_investment + ' and C: ' + this.expected_return + ' and D: ' + this.duration);
+  }
 }
